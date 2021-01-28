ffaf94570985663966f98f8ebc7e6a9e34917b77
ivan-roon <ivan@roonlabs.com>
2021-01-29 01:05:15 +0300

    added an ability to download images

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -5,23 +5,50 @@
 import nock from 'nock';
 import os from 'os';
 import fs from 'fs';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import downloadPage from '../src/index.js';
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = dirname(__filename);
+const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
 const fsPromises = fs.promises;
 let tempTestDir;
+let tempTestFilesDir;
 
 beforeEach(async () => {
-  console.log(path.join(os.tmpdir(), 'page-loader'));
   tempTestDir = await fsPromises.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  tempTestFilesDir = path.join(tempTestDir, 'example-com-files');
 });
 
+const html = `<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>Курсы по программированию Хекслет</title>
+    </head>
+    <body>
+        <img src="/assets/test.png" alt="Иконка профессии Node.js-программист" />
+        <h3>
+            <a href="/professions/nodejs">Node.js-программист</a>
+        </h3>
+    </body>
+</html>`;
+
 test('Is parsed data correct?', async () => {
-  const testString = 'Hello this is me';
+  const expectedImg = await fsPromises.readFile(getFixturePath('test.png'));
   nock('https://example.com')
     .get('/')
-    .reply(200, testString);
+    .reply(200, html)
+    .get('/assets/test.png')
+    .reply(200, expectedImg);
+  console.log(tempTestFilesDir, tempTestDir);
   await downloadPage(tempTestDir, 'https://example.com');
   const result = await fsPromises.readFile(path.join(tempTestDir, 'example-com.html'), 'utf-8');
-  expect(result).toBe(testString);
+  console.log(path.join(tempTestFilesDir, 'example-com-assets-test.png'));
+  const resultImg = await fsPromises.readFile(path.join(tempTestFilesDir, 'example-com-assets-test.png'));
+  expect(resultImg).toEqual(expectedImg);
+  expect(result).toBe(html);
 });
--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
+/* eslint-disable max-len */
 import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
-import URL from 'url';
+import url from 'url';
+import cheerio from 'cheerio';
 
 const fsPromises = fs.promises;
 
 const createFileName = (hostname, pathname) => {
   const formattedHostName = hostname.replace(/\./g, '-');
   const formattedPathName = (pathname === '/' ? '' : pathname.replace(/\//g, '-'));
-  return `${formattedHostName}${formattedPathName}.html`;
+  return `${formattedHostName}${formattedPathName}`;
 };
 
-export default (pathToDirectory, url) => {
-  const parsedURL = URL.parse(url);
+const downloadImage = (sourceURL, pathToDirectory, imageName) => axios
+  .get(sourceURL, { responseType: 'arraybuffer' })
+  .then((response) => {
+    const pathToImage = path.resolve(pathToDirectory, imageName);
+    return fsPromises.writeFile(pathToImage, response.data);
+  });
+
+export default (pathToDirectory, address) => {
+  const parsedURL = url.parse(address);
   const fileName = createFileName(parsedURL.hostname, parsedURL.pathname);
-  const pathToFile = path.resolve(pathToDirectory, fileName);
-  return axios.get(url)
+  const pathToFile = path.resolve(pathToDirectory, `${fileName}.html`);
+  const pathToFilesDir = path.resolve(pathToDirectory, `${fileName}-files`);
+  let links;
+  return axios.get(address)
     .then((response) => {
+      const $ = cheerio.load(response.data);
+      const elements = $('img').toArray();
+      links = elements.map((elem) => {
+        const parsedSRC = url.parse($(elem).attr('src'));
+        if (parsedSRC.hostname === null) {
+          return new URL(parsedSRC.pathname, parsedURL.href);
+        }
+        return parsedSRC;
+      });
       fsPromises.writeFile(pathToFile, response.data, 'utf-8');
+      return fsPromises.mkdir(pathToFilesDir);
     })
+    .then(() => links
+      .map((link) => downloadImage(link.href, pathToFilesDir, createFileName(link.hostname, link.pathname))))
     .then(() => pathToFile);
 };
