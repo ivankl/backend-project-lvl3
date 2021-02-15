48b791288a515fd5ac6a4f5c7b09903b61c1618b
ivan-roon <ivan@roonlabs.com>
2021-02-16 02:46:11 +0300

    another try

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -3,19 +3,17 @@
 import programm from 'commander';
 import downloadPage from '../src/index.js';
 
-export default () => {
-  programm.version('0.0.1')
-    .description('Download a page from Internet and save a file')
-    .arguments('<url>')
-    .option('-o, --output [path]', 'path to file', process.cwd())
-    .action((url) => downloadPage(programm.output, url)
-      .then((resultPath) => {
-        console.log(`Downloaded html file can be found here ${resultPath}`);
-        process.exit();
-      })
-      .catch((error) => {
-        console.error(error.message);
-        process.exit(1);
-      }))
-    .parse(process.argv);
-};
+programm.version('0.0.1')
+  .description('Download a page from Internet and save a file')
+  .arguments('<url>')
+  .option('-o, --output [path]', 'path to file', process.cwd())
+  .action((url) => downloadPage(programm.output, url)
+    .then((resultPath) => {
+      console.log(`Downloaded html file can be found here ${resultPath}`);
+      process.exit();
+    })
+    .catch((error) => {
+      console.error(error.message);
+      process.exit(1);
+    }))
+  .parse(process.argv);
