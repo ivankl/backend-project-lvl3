8fb648af2a5c0c47d7d74762f9b235a63e3b539d
ivan-roon <ivan@roonlabs.com>
2021-02-16 02:43:45 +0300

    another try

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 
-import app from '../src/app.js';
+import programm from 'commander';
+import downloadPage from '../src/index.js';
 
-app();
+export default () => {
+  programm.version('0.0.1')
+    .description('Download a page from Internet and save a file')
+    .arguments('<url>')
+    .option('-o, --output [path]', 'path to file', process.cwd())
+    .action((url) => downloadPage(programm.output, url)
+      .then((resultPath) => {
+        console.log(`Downloaded html file can be found here ${resultPath}`);
+        process.exit();
+      })
+      .catch((error) => {
+        console.error(error.message);
+        process.exit(1);
+      }))
+    .parse(process.argv);
+};
