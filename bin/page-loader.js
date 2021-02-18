043348b3b9151ad98d13f091cf4230dc549b04f1
ivan-roon <ivan@roonlabs.com>
2021-02-19 00:27:40 +0300

    another try

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -5,8 +5,8 @@ import downloadPage from '../src/index.js';
 
 programm.version('0.0.1')
   .description('Download a page from Internet and save a file')
-  .arguments('<url>')
   .option('-o, --output [path]', 'path to file', process.cwd())
+  .arguments('<url>')
   .action((url) => downloadPage(url, programm.output)
     .then((resultPath) => {
       console.log(`Downloaded html file can be found here ${resultPath}`);
@@ -14,6 +14,6 @@ programm.version('0.0.1')
     })
     .catch((error) => {
       console.error(error.message);
-      process.exit(1);
+      process.exitCode = 1;
     }))
   .parse(process.argv);
